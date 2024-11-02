b082ec436c5cd60615983ee2d85df7f744f54c6a
acmors <marcosantoniosilva047@gmail.com>
2024-11-01 21:58:22 -0300

    repository

--- ModeloDeConsulta/src/main/java/com/modelo/repository/ClienteRepository.java
+++ ModeloDeConsulta/src/main/java/com/modelo/repository/ClienteRepository.java
@@ -1,5 +1,11 @@
 package com.modelo.repository;
 
-public class ClienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.modelo.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
 }
